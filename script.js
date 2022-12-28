96e67eaf2bd7a5d97a467a9f2b947f451d9375f6
ashan <indrajee79@gmail.com>
2022-12-28 10:01:26 +0530

    some more js stuff

--- script.js
+++ script.js
@@ -153,3 +153,31 @@ function addToLocalStorage(id, value) {
     localStorage.setItem("list", JSON.stringify(items));
 }
 
+function getLocalStorage() {
+    return localStorage.getItem("list") ? JSON.parse(localStorage.getItem("list")) : [];
+}
+
+function removeFromLocalStorage(id) {
+    let items = getLocalStorage();
+
+    items = items.filter(function (item) {
+        if (item.id !== id) {
+            return item
+        }
+    });
+
+    localStorage.setItem("list", JSON.stringify(items));
+}
+
+function editLocalStorage(id, value) {
+    let items = getLocalStorage();
+
+    items = items.map(function (item) {
+        if (item.id === id) {
+            item.value = value
+        }
+        return item;
+    });
+    localStorage.setItem("list", JSON.stringify(items));
+}
+
